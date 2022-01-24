1770daafdc78ae5cc1ab97766b47fabb537ce35e
Marc Quesnel <93592810+MarcQuesnel@users.noreply.github.com>
2022-01-23 20:41:49 -0400

    Homework
    Homework practice.js

---
+++ practice.js
@@ -0,0 +1,25 @@
+/* Marc Quesnel
+01/13/2022 Home Work Assigment
+Java Script Programming
+*/
+
+let computerModel = 'MacBook_Pro_M1';
+let salePrice = '2399.99';
+let taxRate = '.15';
+let calculatedTax = salePrice * taxRate;
+let totalPrice = saleprice * 1.15;
+
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log('               Computer Sale                    ');
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log('~~~~~~~~~~~~~~~Computer Model~~~~~~~~~~~~~~~~~~~');
+console.log(computerModel);
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log('~~~~~~~~~~~~~~~Sale Price~~~~~~~~~~~~~~~~~~~~~~~');
+console.log(salePrice);
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log('~~~~~~~~~~~~~~~~~Calculated Tax~~~~~~~~~~~~~~~~~');
+console.log(calculatedTax);
+console.log('~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~');
+console.log('~~~~~~~~~~~~~~~~~Total Price~~~~~~~~~~~~~~~~~~~~');
+console.log(totalPrice);
